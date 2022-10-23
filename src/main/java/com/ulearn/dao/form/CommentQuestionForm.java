19bf8b9d1111ea95ed6f2d9db2927f67475fc5a0
leeryan2000 <ryanlee4d@hotmail.com>
2022-10-23 15:50:21 +0800

    Project restructured

---
+++ src/main/java/com/ulearn/dao/form/CommentQuestionForm.java
@@ -0,0 +1,27 @@
+package com.ulearn.dao.form;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+/**
+ * @Author: Ryan
+ * @Description:
+ * @Date: 2022/9/22 22:25
+ */
+
+@Data
+@Schema(description = "用户评论问题格式")
+public class CommentQuestionForm {
+
+    @NotNull(message = "问题ID不能为空")
+    private Long questionId;
+
+    @NotBlank(message = "评论不能为空")
+    private String content;
+
+    @Schema(description = "回复的用户ID, 默认为空")
+    private Long replyId;
+}
